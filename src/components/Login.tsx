9e07d382695590b193333c11750613029548081c
newy12 <newy12@naver.com>
2024-03-11 09:41:21 +0900

    #feat. test1

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -48,7 +48,7 @@ export default function Login() {
         }
     }
 
-    return (//d
+    return (
         <div className="w-full text-center mt-20">
             <div className="w-80 mx-auto border border-solid border-gray-200 rounded-lg p-8">
                 <h2 className="text-lg">로그인</h2>
